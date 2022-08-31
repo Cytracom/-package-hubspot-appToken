ae5a5dd34a384168a6fef44845429572893b5a86
Conrad Grant <imovertheinternet@gmail.com>
2022-08-31 14:14:09 -0500

    removing stuff cause php5.5

--- src/Api/CrmAssociations.php
+++ src/Api/CrmAssociations.php
@@ -8,45 +8,6 @@ namespace Fungku\HubSpot\Api;
  */
 class CrmAssociations extends Api
 {
-    public const CONTACT_TO_COMPANY = 1;
-    public const COMPANY_TO_CONTACT = 2;
-    public const DEAL_TO_CONTACT = 3;
-    public const CONTACT_TO_DEAL = 4;
-    public const DEAL_TO_COMPANY = 5;
-    public const COMPANY_TO_DEAL = 6;
-    public const COMPANY_TO_ENGAGEMENT = 7;
-    public const ENGAGEMENT_TO_COMPANY = 8;
-    public const CONTACT_TO_ENGAGEMENT = 9;
-    public const ENGAGEMENT_TO_CONTACT = 10;
-    public const DEAL_TO_ENGAGEMENT = 11;
-    public const ENGAGEMENT_TO_DEAL = 12;
-    public const PARENT_COMPANY_TO_CHILD_COMPANY = 13;
-    public const CHILD_COMPANY_TO_PARENT_COMPANY = 14;
-    public const CONTACT_TO_TICKET = 15;
-    public const TICKET_TO_CONTACT = 16;
-    public const TICKET_TO_ENGAGEMENT = 17;
-    public const ENGAGEMENT_TO_TICKET = 18;
-    public const DEAL_TO_LINE_ITEM = 19;
-    public const LINE_ITEM_TO_DEAL = 20;
-    public const COMPANY_TO_TICKET = 25;
-    public const TICKET_TO_COMPANY = 26;
-    public const DEAL_TO_TICKET = 27;
-    public const TICKET_TO_DEAL = 28;
-    public const ADVISOR_TO_COMPANY = 33;
-    public const COMPANY_TO_ADVISOR = 34;
-    public const BOARD_MEMBER_TO_COMPANY = 35;
-    public const COMPANY_TO_BOARD_MEMBER = 36;
-    public const CONTRACTOR_TO_COMPANY = 37;
-    public const COMPANY_TO_CONTRACTOR = 38;
-    public const MANAGER_TO_COMPANY = 39;
-    public const COMPANY_TO_MANAGER = 40;
-    public const BUSINESS_OWNER_TO_COMPANY = 41;
-    public const COMPANY_TO_BUSINESS_OWNER = 42;
-    public const PARTNER_TO_COMPANY = 43;
-    public const COMPANY_TO_PARTNER = 44;
-    public const RESELLER_TO_COMPANY = 45;
-    public const COMPANY_TO_RESELLER = 46;
-
     /**
      * Get associations for a CRM object.
      *
